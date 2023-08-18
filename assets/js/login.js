e1fcab5c5f00006a8394d4c88498dbbfb357dd7b
nguyenducan1909 <nguyenducan2004919@gmail.com>
2023-08-18 22:43:45 +0700

    second commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,43 +1,11 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-    var $this = $(this),
-        label = $this.prev('label');
-  
-        if (e.type === 'keyup') {
-              if ($this.val() === '') {
-            label.removeClass('active highlight');
-          } else {
-            label.addClass('active highlight');
-          }
-      } else if (e.type === 'blur') {
-          if( $this.val() === '' ) {
-              label.removeClass('active highlight'); 
-              } else {
-              label.removeClass('highlight');   
-              }   
-      } else if (e.type === 'focus') {
-        
-        if( $this.val() === '' ) {
-              label.removeClass('highlight'); 
-              } 
-        else if( $this.val() !== '' ) {
-              label.addClass('highlight');
-              }
+$(document).ready(function(){
+  $('#eye').click(function(){
+      $(this).toggleClass('open');
+      $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+      if($(this).hasClass('open')){
+          $(this).prev().attr('type', 'text');
+      }else{
+          $(this).prev().attr('type', 'password');
       }
-  
   });
-  
-  $('.tab a').on('click', function (e) {
-    
-    e.preventDefault();
-    
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    
-    target = $(this).attr('href');
-  
-    $('.tab-content > div').not(target).hide();
-    
-    $(target).fadeIn(600);
-    
-  });
+});
